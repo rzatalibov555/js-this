60ad6e4d46c050af4d99ee05bdd559744cefa516
rzatalibov555 <sethubpro@gmail.com>
2023-03-02 22:38:32 +0400

    this

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+function getData(e){
+    e.style.background = "blue"
+}
+function removeData(e){
+    e.style.background = "red"
+}
+function radiusData(e){
+    e.style.background = "transparent"
+}
+
+function no_radiusData(e){
+    e.style.borderRadius = "0"
+}
+
+
---
+++ js/new.js
@@ -0,0 +1,5 @@
+
+function get_car(){
+    let car = document.getElementById('car').value
+    document.getElementById('myCar').src = "img/"+car
+}
